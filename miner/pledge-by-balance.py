c829fcad46a1e9ffd7dd3931e6c790238ab5885d
aimkiray <aimkiray@MacBook-Pro.local>
2020-12-23 14:50:34 +0800

    feat: parallel & auto balance

--- miner/pledge-by-balance.py
+++ miner/pledge-by-balance.py
@@ -7,22 +7,25 @@ import os
 import subprocess as sp
 import logging
 
-LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
-DATE_FORMAT = "%m-%d-%Y %H:%M:%S"
+auto_withdraw_balance = 5
+auto_pledge_balance = 5
+parallel_jobs = 5
 
-logging.basicConfig(filename='pledge.log', level=logging.INFO,
-                    format=LOG_FORMAT, datefmt=DATE_FORMAT)
-
-daemon_url = "http://172.16.119.1:1234/rpc/v0"
-daemon_token = ""
+daemon_url = "http://127.0.0.1:1234/rpc/v0"
+daemon_token = "your daemon token here"
 
 miner_url = "http://172.16.119.1:2345/rpc/v0"
-miner_token = ""
+miner_token = "your miner token here"
 
 os.environ["LOTUS_PATH"] = "/tank1/filecash/.lotus"
 os.environ["LOTUS_MINER_PATH"] = "/tank1/filecash/.lotusminer"
 os.environ["FIL_PROOFS_PARAMETER_CACHE"] = "/tank1/filecash-proof-parameters"
 
+LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+DATE_FORMAT = "%m-%d-%Y %H:%M:%S"
+
+logging.basicConfig(filename='pledge.log', level=logging.INFO,
+                    format=LOG_FORMAT, datefmt=DATE_FORMAT)
 
 # Call any supported API
 def call_any(target, method, params):
@@ -64,34 +67,47 @@ if __name__ == "__main__":
     miner_wallet = call_any("miner", "ActorAddress", None)["result"]
     miner_balance_verbose = call_any("daemon", "StateMinerAvailableBalance", [miner_wallet, []])["result"]
     miner_balance = int(int(miner_balance_verbose)/(10**18))
-    logging.info("Miner balance: " + miner_balance)
-    if miner_balance > 1:
+    logging.info("Miner balance: " + str(miner_balance))
+    if miner_balance > auto_withdraw_balance:
         # Withdraw balance, there is no RPC method
         msg_id_verbose = sp.Popen(["./lotus-miner", "actor", "withdraw"], stdout=sp.PIPE, stderr=sp.PIPE).stdout
+        msg_id_verbose.wait()
         msg_id = msg_id_verbose.readline().decode(encoding='UTF-8').strip().split(" ")[-1]
         # Wait a moment please
-        sp.Popen(["./lotus", "state", "wait-msg", msg_id])
+        sp.call(["./lotus", "state", "wait-msg", msg_id])
 
     # Check current jobs
     pre_jobs_count = 0
+    cur_jobs_count = 0
+    av_jobs_count = 0
     worker_jobs = call_any("miner", "WorkerJobs", None)["result"]
-    for key, jobs in worker_jobs:
+    for key, jobs in worker_jobs.items():
+        # Miner
+        if key == "0":
+            continue
+        # Worker
         for job in jobs:
+            cur_jobs_count += 1
             if "precommit" in job["Task"]:
                 pre_jobs_count += 1
-    logging.info("PreCommit jobs: " + pre_jobs_count)
+        av_jobs_count += parallel_jobs - cur_jobs_count
+    logging.info("PreCommit jobs: " + str(pre_jobs_count))
 
     # Check wallet balance
     def_wallet = call_any("daemon", "WalletDefaultAddress", None)["result"]
     def_balance_verbose = call_any("daemon", "WalletBalance", [def_wallet])["result"]
     def_balance = int(int(def_balance_verbose)/(10**18))
-    logging.info("Available balance: " + def_balance)
+    logging.info("Available balance: " + str(def_balance))
 
-    # If balance - precommit > 1, lol
+    # If balance - precommit > auto_pledge_balance, lol
     count = def_balance - pre_jobs_count
-    if count >= 1 :
+    if count > auto_pledge_balance :
         for i in range(count):
+            # Remaining workload
+            if av_jobs_count < 1:
+                break
             logging.info("Make a sector.")
-            sp.Popen(["./lotus-miner", "sectors", "pledge"])
+            sp.call(["./lotus-miner", "sectors", "pledge"])
+            av_jobs_count -= 1
             # Wait for add piece
             time.sleep(900)
