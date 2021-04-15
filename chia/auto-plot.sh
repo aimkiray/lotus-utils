dd25678dba85399203548886081231f666233933
aimkiray <root@meowwoo.com>
2021-04-15 14:49:38 +0800

    feat: set cannot exceed actual

--- chia/auto-plot.sh
+++ chia/auto-plot.sh
@@ -3,23 +3,23 @@
 # This is a auto plot script for chia.
 
 PLOT_SIZE=110
-PLOT_PATH_LIMIT=15000
-CACHE_SIZE=350
+PLOT_PATH_LIMIT=7200
+CACHE_SIZE=300
 CACHE_PATH_LIMIT=14000
-PARALLEL_PLOT=20
+PARALLEL_PLOT=24
 # chia keys show
 FARMER_PK=xxx
 POOL_PK=xxx
 
 # NFS mount BASE path
-NFS_BASE_PATH=/nfs230
-NFS_INDEX_FIRST=20
-NFS_INDEX_LAST=20
+NFS_BASE_PATH=/nfs990
+NFS_INDEX_FIRST=1
+NFS_INDEX_LAST=1
 # Mount BASE path for each drive in the NFS path
 # e.g. /nfs23020/test9
 DRIVE_BASE_PATH=/test
-DRIVE_INDEX_FIRST=9
-DRIVE_INDEX_LAST=18
+DRIVE_INDEX_FIRST=1
+DRIVE_INDEX_LAST=24
 # SSD cache path
 CACHE_PATH=/tank1/test
 
@@ -84,6 +84,13 @@ while true; do
         _error "Cache path $CACHE_PATH 404, self explosion."
     fi
 
+    CACHE_PATH_LIMIT_TRUE=$(df -P $CACHE_PATH | awk 'NR==2 {print $2}')
+    CACHE_PATH_LIMIT_TRUE=$(($CACHE_PATH_LIMIT_TRUE / 1024 / 1024))
+    if [ $CACHE_PATH_LIMIT_TRUE -lt $CACHE_PATH_LIMIT ]; then
+        (($CACHE_PATH_LIMIT = $CACHE_PATH_LIMIT_TRUE))
+        _warn "The real capacity of cache is ${CACHE_PATH_LIMIT_TRUE}GB, but the configuration is ${CACHE_PATH_LIMIT}GB"
+    fi
+
     # Loop NFS path
     for ((i = $NFS_INDEX_FIRST; i <= $NFS_INDEX_LAST; i++)); do
 
@@ -97,15 +104,15 @@ while true; do
                 continue
             fi
 
-            CUR_PLOT=$(ps -ef | grep "chia plots create" | wc -l)
+            CUR_PLOT=$(ps -ef | grep "chia plots create" | grep -v grep | wc -l)
             if [ $CUR_PLOT -ge $PARALLEL_PLOT ]; then
                 _warn "Currently $CUR_PLOT parallel plot is overload, sleep."
                 break
             fi
 
             # Calculate the cache required for all running plots.
-            CACHE_PATH_SIZE=$(du -s $CACHE_PATH | awk '{print $1}')
-            CACHE_PATH_SIZE=$(($CACHE_PATH_SIZE / 2 ** 21))
+            CACHE_PATH_SIZE=$(df -P $CACHE_PATH | awk 'NR==2 {print $3}')
+            CACHE_PATH_SIZE=$(($CACHE_PATH_SIZE / 1024 / 1024))
             CACHE_RUNING_OCC=$(($CUR_PLOT * $CACHE_SIZE))
             CACHE_PATH_SIZE_REMAIN=$(($CACHE_PATH_LIMIT - $CACHE_PATH_SIZE - $CACHE_RUNING_OCC))
 
@@ -125,9 +132,16 @@ while true; do
                 continue
             fi
 
+            PLOT_PATH_LIMIT_TRUE=$(df -P $TARGET_PATH | awk 'NR==2 {print $2}')
+            PLOT_PATH_LIMIT_TRUE=$(($PLOT_PATH_LIMIT_TRUE / 1024 / 1024))
+            if [ $PLOT_PATH_LIMIT_TRUE -lt $PLOT_PATH_LIMIT ]; then
+                (($PLOT_PATH_LIMIT = $PLOT_PATH_LIMIT_TRUE))
+                _warn "The real capacity of plot path is ${PLOT_PATH_LIMIT_TRUE}GB, but the configuration is ${PLOT_PATH_LIMIT}GB"
+            fi
+
             # Calculate the space for all existing plots.
-            TARGET_PATH_SIZE=$(du -s $TARGET_PATH | awk '{print $1}')
-            TARGET_PATH_SIZE=$(($TARGET_PATH_SIZE / 2 ** 21))
+            TARGET_PATH_SIZE=$(df -P $TARGET_PATH | awk 'NR==2 {print $3}')
+            TARGET_PATH_SIZE=$(($TARGET_PATH_SIZE / 1024 / 1024))
             TARGET_PATH_SIZE_ALL=$(($TARGET_PATH_SIZE + $TARGET_PATH_OCC_SIZE))
             TARGET_PATH_SIZE_REMAIN=$(($PLOT_PATH_LIMIT - $TARGET_PATH_SIZE_ALL))
 
@@ -140,6 +154,7 @@ while true; do
             CUR_TIME=$(date "+%Y%m%d-%H%M%S")
             mkdir -p log
             nohup chia plots create -n 1 -u 64 -r 2 -k 32 -b 8000 -t $CACHE_PATH -d $TARGET_PATH -f $FARMER_PK -p $POOL_PK >>log/plot-$CUR_TIME.log 2>&1 &
+            _info "Plot will be placed in the ${TARGET_PATH} directory."
 
             # Get plot ID.
             sleep 3s
